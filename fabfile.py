3d7fc0f24864cd05b61be0eb65759293adbf5118
olegush <oleg107117@gmail.com>
2019-08-07 11:34:46 +0300

    fabfile updates

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,6 @@
 import os
 
 from environs import Env
-from dotenv import load_dotenv
 from fabric import Connection
 from fabric import task
 import fabric
@@ -25,16 +24,16 @@ ENV_DIR = env('ENV_DIR')
 
 def user_exists(c, user):
     result = c.run(f'sudo -i -u postgres psql -t -A -c "SELECT COUNT(*) FROM pg_user WHERE usename=\'{user}\';"')
-    return result.__dict__['stdout'].strip() == '1'
+    return result.stdout.strip() == '1'
 
 
 def db_exists(c, db):
     result = c.run(f'sudo -i -u postgres psql -t -A -c "SELECT datname FROM pg_catalog.pg_database WHERE datname = \'{db}\';"')
-    return result.__dict__['stdout'].strip() == db
+    return result.stdout.strip() == db
 
 
 @task()
-def deploy(c):
+def bootstrap(c):
     # Install packages.
     c.run('apt-get update')
     c.run('apt install python3-pip python3-dev python3-psycopg2 libpq-dev postgresql-10 postgresql-contrib nginx curl')
@@ -66,17 +65,6 @@ def deploy(c):
         c.run(f'[ -d {ENV_DIR} ] || virtualenv {ENV_DIR}')
         c.run(f'source {ENV_DIR}/bin/activate && python3 -m pip install -r requirements.txt')
 
-        # Git pull
-        c.run(f'git config --global user.email "{GITHUB_EMAIL}"')
-        c.run(f'git config --global user.name "{GITHUB_NAME}"')
-        c.run('git pull')
-
-        # Run Django management commands.
-        c.run(f'python3 {DIR_PROJECT}/manage.py makemigrations')
-        c.run(f'python3 {DIR_PROJECT}/manage.py migrate')
-        c.run(f'python3 {DIR_PROJECT}/manage.py migrate')
-        c.run(f'python3 {DIR_PROJECT}/manage.py collectstatic')
-
         # Set Gunicorn configs
         c.run('envsubst < conf_templates/gunicorn.socket.template > /etc/systemd/system/gunicorn.socket')
         c.run(f'export DIR_SOURCES={DIR_SOURCES} && export DIR_PROJECT={DIR_PROJECT} && export DIR_PACKAGE={DIR_PACKAGE} && export ENV_DIR={ENV_DIR} && envsubst < conf_templates/gunicorn.service.template > /etc/systemd/system/gunicorn.service')
@@ -88,8 +76,23 @@ def deploy(c):
         c.run('systemctl restart gunicorn')
 
         # Set Nginx configs
-        c.run(f'export DIR_SOURCES={DIR_SOURCES} && export DIR_PROJECT={DIR_PROJECT} && export SITE={SITE} && envsubst < conf_templates/nginx.conf.template > /etc/nginx/sites-available/{DIR_PROJECT}.conf')
+        c.run(f'export DIR_SOURCES={DIR_SOURCES} && export DIR_PROJECT={DIR_PROJECT} && export SITE={SITE} && export KEY=\'$request_uri\' && envsubst < conf_templates/nginx.conf.template > /etc/nginx/sites-available/{DIR_PROJECT}.conf')
         c.run(f'ln -sf /etc/nginx/sites-available/{DIR_PROJECT}.conf /etc/nginx/sites-enabled')
         c.run('ufw delete allow 8000')
         c.run('ufw allow "Nginx Full"')
         c.run('systemctl restart nginx')
+
+
+@task()
+def deploy(c):
+    with c.cd(DIR_SOURCES):
+        c.run(f'source {ENV_DIR}/bin/activate')
+
+        # Git pull
+        c.run(f'git config --global user.email "{GITHUB_EMAIL}"')
+        c.run(f'git config --global user.name "{GITHUB_NAME}"')
+        c.run('git pull')
+
+        # Run Django management commands.
+        c.run(f'python3 {DIR_PROJECT}/manage.py migrate')
+        c.run(f'python3 {DIR_PROJECT}/manage.py collectstatic')
