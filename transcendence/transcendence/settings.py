eb31117f86662ccc4ecf59b037d569acfa32d5fa
olegush <oleg107117@gmail.com>
2019-08-21 10:49:45 +0300

    static changed

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -136,8 +136,8 @@ class Dev(Configuration):
 
     STATIC_URL = '/static/'
 
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
