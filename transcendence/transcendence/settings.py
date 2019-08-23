52511a33ab589fc48d1438977d1da5ca16594e57
olegush <oleg107117@gmail.com>
2019-08-23 11:58:25 +0300

    media edited

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -139,6 +139,6 @@ class Dev(Configuration):
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = 'media/' #os.path.join(BASE_DIR, 'media/')
 
-    MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
