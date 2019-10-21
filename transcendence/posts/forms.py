da92f52e29a67d42efee84eb6ad28459d2125947
olegush <oleg107117@gmail.com>
2019-10-21 10:47:11 +0300

    flaked form

--- transcendence/posts/forms.py
+++ transcendence/posts/forms.py
@@ -1,15 +1,15 @@
 from django.forms import ModelForm, Textarea, TextInput, FileInput
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django import forms
 
 from posts.models import Post
 
 
 class PostForm(ModelForm):
+    """Create user post."""
+
     class Meta:
-       model = Post
-       fields = ['name', 'description']
-       widgets = {
+        model = Post
+        fields = ['name', 'description']
+        widgets = {
             'name': TextInput(attrs={'class': 'form-control'}),
             'description': Textarea(attrs={'rows': 10, 'class': 'form-control'}),
-        }
+            }
--- transcendence/users/forms.py
+++ transcendence/users/forms.py
@@ -1,27 +1,32 @@
-from django.forms import ModelForm, Textarea, TextInput, FileInput
+from django.forms import ModelForm, Textarea, TextInput
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django import forms
 
 from users.models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """Create custom user."""
+
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ('email',)
 
 
 class CustomUserChangeForm(UserChangeForm):
+    """Edit custom user."""
+
     class Meta:
         model = CustomUser
         fields = ('email',)
 
 
 class ProfileForm(ModelForm):
+    """Edit user info."""
+
     class Meta:
-       model = CustomUser
-       fields = ['name', 'description', 'image']
-       widgets = {
+        model = CustomUser
+        fields = ['name', 'description', 'image']
+        widgets = {
             'name': TextInput(attrs={'class': 'form-control'}),
             'description': Textarea(attrs={'rows': 10, 'class': 'form-control'}),
-        }
+            }
