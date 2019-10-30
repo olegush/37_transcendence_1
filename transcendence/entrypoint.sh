3039b1e22b2f5e5606468f3518ad00ab68c2d3f0
olegush <oleg107117@gmail.com>
2019-10-30 11:48:32 +0300

    fixed dockerize

--- transcendence/entrypoint.sh
+++ transcendence/entrypoint.sh
@@ -14,5 +14,6 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('superuser@superuser.com', 'pwd12345')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_user('user@user.com', 'pwd123')" | python manage.py shell
 
 exec "$@"
--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -50,7 +50,7 @@ class Dev(Configuration):
         'default': {
             'BACKEND': 'channels_redis.core.RedisChannelLayer',
             'CONFIG': {
-                "hosts": [env("HOSTS"), 6379],
+                "hosts": [(env("HOSTS"), 6379)],
             },
         },
     }
